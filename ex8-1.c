faa2c12a9ccfd429a683c2d0cf015c4600275080
Seoyeong Joo <w2017@e-mirim.hs.kr>
2021-10-26 01:25:59 +0900

    ch08
    ch08

---
+++ ex8-1.c
@@ -0,0 +1,17 @@
+#define_
+#include <stdio.h>
+
+int main(void) {
+	int ary[5];
+	
+	ary[0]=10;
+	ary[1] = 20;
+	ary[2] = ary[0] + ary[1];
+	scanf("%d", &ary[3]);
+
+	printf("%d\n", ary[2]);
+	printf("%d\n", ary[3]);
+	printf("%d\n", ary[4]);
+
+	return 0;
+}
